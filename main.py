d0723167158fd97cfb9397f92b4e47a08789e27d
jumahi <mrjasonu@gmail.com>
2021-07-03 14:48:32 +0100

    The project is done and has been tested and does not produce errors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def login(name, account_no):
     with open('transactions.txt','a') as trans:
         real_account_no = "******" + str(account_no)[-3:]
-        trans.write('\n' +"-"*50)
+        trans.write('\n'+"-"*50)
         trans.write('\n'+"-"*50)
         trans.write(f"\n{name} just logged into his account and his account number is {real_account_no}.")
     return name
@@ -43,3 +43,45 @@ def check_balance():
         trans.write("\n"+"-"*50)
         trans.write(f"\n{name} just checked his account balance and it was ${balance}.")
 
+def start(user_input):
+    if user_input == str(1) or user_input == "CHECK ACCOUNT BALANCE":
+        print("\nWELCOME TO E-BANK CHECK BALANCE!")
+        check_balance()
+    elif user_input == str(2) or user_input == "SEND MONEY":
+        print("\nWELCOME TO E-BANK SEND MONEY!")
+        to = input("Whom would you like to send money to? ")
+        amount = input("Amount: ")
+        send_money(to, amount)
+    elif user_input == str(3) or user_input == "BORROW MONEY":
+        print("\nWELCOME TO E-BANK BORROW!")
+        amount = input("How much would you like to borrow: ")
+        collateral = input("What would you like to use as collateral: ")
+        if collateral == "":
+            borrow_money(amount)
+        else:
+            borrow_money(amount, collateral)
+
+
+print("Welcome to E-Bank")
+
+name = str(input("\nName: "))
+acc_no = str(input("Account Number: "))
+login(name, acc_no)
+
+print(
+"""
+1.CHECK ACCOUNT BALANCE
+2.SEND MONEY
+3.BORROW MONEY
+""")
+
+
+run = 0
+while run == 0:
+    user_input = input("WHAT WOULD YOU LIKE TO DO? ")
+    if user_input not in ["1","2","3"] and user_input not in ["CHECK ACCOUNT BALANCE", "SEND MONEY", "BORROW MONEY"]:
+        run = 1
+    else:
+        start(user_input)
+else:
+    print("GOODBYE!")
